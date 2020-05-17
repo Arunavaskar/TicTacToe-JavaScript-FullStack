cbfb04671bfa488315626ee2d3d3dae517fd736f
Arunava Chakraborty <arunavaskar@outlook.com>
2020-05-17 20:55:36 +0530

    done

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 // here we have to know when the grid/ boxes are being clicked.
+$(document).ready(()=>{
 
+    $(".one").click(() => {
+        $(".one").text("X");
+    });
+    $(".two").click(() => {
+        alert("two clicked!");
+    });
+    $(".three").click(() => {
+        alert("three clicked!");
+    });
+    $(".four").click(() => {
+        alert("four clicked!");
+    });
+    $(".five").click(() => {
+        alert("five clicked!");
+    });
+    $(".six").click(() => {
+        alert("six clicked!");
+    });
+    $(".seven").click(() => {
+        alert("seven clicked!");
+    });
+    $(".eight").click(() => {
+        alert("eight clicked!");
+    });
+    $(".nine").click(() => {
+        alert("nine clicked!");
+    });
+
+});
